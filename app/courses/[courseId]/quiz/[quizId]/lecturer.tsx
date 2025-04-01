182a9650323e81ff5547e6af9eaa4c111a5cc31f
Jannik Becker <st177878@stud.uni-stuttgart.de>
2025-04-01 22:39:25 +0200

    refactor: header & error handling of quiz lecturer view

--- app/courses/[courseId]/quiz/[quizId]/lecturer.tsx
+++ app/courses/[courseId]/quiz/[quizId]/lecturer.tsx
@@ -1,7 +1,5 @@
 import { lecturerEditQuizQuery } from "@/__generated__/lecturerEditQuizQuery.graphql";
-import { ContentTags } from "@/components/ContentTags";
-import { FormErrors } from "@/components/FormErrors";
-import { Heading } from "@/components/Heading";
+import { ES2022Error } from "@/components/FormErrors";
 import { PageError } from "@/components/PageError";
 import { QuizModal } from "@/components/QuizModal";
 import { ItemData } from "@/components/form-sections/ItemFormSection";
@@ -9,80 +7,86 @@ import { AddQuestionButton } from "@/components/quiz/AddQuestionButton";
 import { AssociationQuestionPreview } from "@/components/quiz/AssociationQuestionPreview";
 import { ClozeQuestionPreview } from "@/components/quiz/ClozeQuestionPreview";
 import { DeleteQuestionButton } from "@/components/quiz/DeleteQuestionButton";
-import { DeleteQuizButton } from "@/components/quiz/DeleteQuizButton";
 import { EditAssociationQuestionButton } from "@/components/quiz/EditAssociationQuestionButton";
 import { EditClozeQuestionButton } from "@/components/quiz/EditClozeQuestionButton";
 import { EditMultipleChoiceQuestionButton } from "@/components/quiz/EditMultipleChoiceQuestionButton";
+import LecturerQuizHeader from "@/components/quiz/LecturerQuizHeader";
 import { MultipleChoiceQuestionPreview } from "@/components/quiz/MultipleChoiceQuestionPreview";
-import { Edit } from "@mui/icons-material";
-import { Button } from "@mui/material";
-import { useParams, useRouter } from "next/navigation";
-import { useState } from "react";
+import { useParams } from "next/navigation";
+import { createContext, useContext, useState } from "react";
 import { graphql, useLazyLoadQuery } from "react-relay";
-export default function LecturerQuiz() {
-  const { quizId, courseId } = useParams();
-  const router = useRouter();
-  const [error, setError] = useState<any>(null);
 
-  const { contentsByIds, ...query } = useLazyLoadQuery<lecturerEditQuizQuery>(
-    graphql`
-      query lecturerEditQuizQuery($id: UUID!, $courseId: UUID!) {
-        ...MediaRecordSelector
-        contentsByIds(ids: [$id]) {
+const rootQuery = graphql`
+  query lecturerEditQuizQuery($id: UUID!, $courseId: UUID!) {
+    ...MediaRecordSelector
+    contentsByIds(ids: [$id]) {
+      ...LecturerQuizHeaderFragment
+
+      id
+      metadata {
+        name
+        chapterId
+        ...ContentTags
+      }
+      ... on QuizAssessment {
+        quiz {
+          assessmentId
+          questionPool {
+            itemId
+            type
+            number
+            ...MultipleChoiceQuestionPreviewFragment
+            ...ClozeQuestionPreviewFragment
+            ...EditMultipleChoiceQuestionButtonFragment
+            ...AssociationQuestionPreviewFragment
+            ...EditClozeQuestionButtonFragment
+            ...EditAssociationQuestionButtonFragment
+          }
+          ...QuizModalFragment
+        }
+        items {
           id
-          metadata {
-            name
-            chapterId
-            ...ContentTags
+          associatedSkills {
+            id
+            skillName
+            skillCategory
+            isCustomSkill
           }
-          ... on QuizAssessment {
-            quiz {
-              assessmentId
-              questionPool {
-                itemId
-                type
-                number
-                ...MultipleChoiceQuestionPreviewFragment
-                ...ClozeQuestionPreviewFragment
-                ...EditMultipleChoiceQuestionButtonFragment
-                ...AssociationQuestionPreviewFragment
-                ...EditClozeQuestionButtonFragment
-                ...EditAssociationQuestionButtonFragment
-              }
-              ...QuizModalFragment
-            }
-            items {
-              id
-              associatedSkills {
-                id
-                skillName
-                skillCategory
-                isCustomSkill
-              }
-              associatedBloomLevels
-            }
-          }
+          associatedBloomLevels
         }
       }
-    `,
-    { id: quizId, courseId }
+    }
+  }
+`;
+
+interface ErrorContextProps {
+  error: ES2022Error | null;
+  setError: (error: ES2022Error | null) => void;
+}
+const ErrorContext = createContext<ErrorContextProps>({
+  error: null,
+  setError: () => {},
+});
+export const useError = () => useContext(ErrorContext);
+
+export default function LecturerQuiz() {
+  const { quizId, courseId } = useParams();
+  const [error, setError] = useState<ES2022Error | null>(null);
+
+  // TODO `...query` should be removed after clean relay.js fragment refactoring
+  const { contentsByIds, ...query } = useLazyLoadQuery<lecturerEditQuizQuery>(
+    rootQuery,
+    {
+      id: quizId,
+      courseId,
+    }
   );
+
   const [isEditSetOpen, setEditSetOpen] = useState(false);
 
   const content = contentsByIds[0];
-  if (!content) {
-    return <PageError message="No quiz found with given id." />;
-  }
-
   const quiz = content.quiz;
-  if (!quiz) {
-    return (
-      <PageError
-        title={contentsByIds[0].metadata.name}
-        message="Content not of type quiz."
-      />
-    );
-  }
+
   function getItem(itemId: string): ItemData {
     if (content && content.items) {
       for (let i = 0; i < content.items.length; i++) {
@@ -98,110 +102,103 @@ export default function LecturerQuiz() {
     return { associatedBloomLevels: [], associatedSkills: [] };
   }
 
+  if (!quiz) {
+    return (
+      <PageError
+        title={content.metadata.name}
+        message="Content not of type quiz."
+      />
+    );
+  }
+
   return (
     <main>
-      <FormErrors error={error} onClose={() => setError(null)} />
-      <Heading
-        title={content.metadata.name}
-        action={
-          <div className="flex gap-2">
-            <DeleteQuizButton
-              chapterId={content.metadata.chapterId}
-              contentId={content.id}
-              onCompleted={() => router.push(`/courses/${courseId}`)}
-              onError={setError}
-            />
-            <Button
-              sx={{ color: "text.secondary" }}
-              startIcon={<Edit />}
-              onClick={() => setEditSetOpen(true)}
-            >
-              Edit
-            </Button>
-          </div>
-        }
-        backButton
-      />
-      <ContentTags metadata={content.metadata} />
-
-      {quiz.questionPool.map((question, index) => (
-        <div
-          key={question.itemId}
-          className="my-3 py-3 border-b flex justify-between items-start"
-        >
-          {question.type === "MULTIPLE_CHOICE" && (
-            <>
-              <MultipleChoiceQuestionPreview _question={question} />
-              <div className="flex">
-                <EditMultipleChoiceQuestionButton
-                  _allRecords={query}
-                  _question={question}
-                  assessmentId={content.id!}
-                  courseId={courseId}
-                  item={getItem(question.itemId)}
-                />
-                <DeleteQuestionButton
-                  assessmentId={content.id!}
-                  questionId={question.itemId}
-                  num={question.number}
-                />
-              </div>
-            </>
-          )}
-          {question.type === "CLOZE" && (
-            <>
-              <ClozeQuestionPreview _question={question} />
-              <div className="flex">
-                <EditClozeQuestionButton
-                  _allRecords={query}
-                  _question={question}
-                  assessmentId={content.id}
-                  courseId={courseId}
-                  item={getItem(question.itemId)}
-                />
-                <DeleteQuestionButton
-                  assessmentId={content.id}
-                  questionId={question.itemId}
-                  num={question.number}
-                />
-              </div>
-            </>
-          )}
-          {question.type === "ASSOCIATION" && (
-            <>
-              <AssociationQuestionPreview _question={question} />
-              <div className="flex">
-                <EditAssociationQuestionButton
-                  _allRecords={query}
-                  _question={question}
-                  assessmentId={content.id}
-                  courseId={courseId}
-                  item={getItem(question.itemId)}
-                />
-                <DeleteQuestionButton
-                  assessmentId={content.id}
-                  questionId={question.itemId}
-                  num={question.number}
-                />
-              </div>
-            </>
-          )}
-        </div>
-      ))}
-      <div className="mt-8 flex flex-col items-start">
-        <AddQuestionButton
-          _allRecords={query}
-          assessmentId={content.id}
-          courseId={courseId}
+      <ErrorContext.Provider value={{ error, setError }}>
+        <LecturerQuizHeader
+          openEditQuizModal={() => setEditSetOpen(true)}
+          content={content}
         />
-      </div>
 
-      <QuizModal
-        onClose={() => setEditSetOpen(false)}
-        isOpen={isEditSetOpen}
-        _existingQuiz={quiz}
-        chapterId={content.metadata.chapterId}
-      />
+        {quiz.questionPool.map((question, i) => (
+          <div
+            key={question.itemId}
+            className="my-3 py-3 border-b flex justify-between items-start"
+          >
+            {question.type === "MULTIPLE_CHOICE" && (
+              <>
+                <MultipleChoiceQuestionPreview _question={question} />
+                <div className="flex">
+                  <EditMultipleChoiceQuestionButton
+                    _allRecords={query}
+                    _question={question}
+                    assessmentId={content.id!}
+                    courseId={courseId}
+                    item={getItem(question.itemId)}
+                  />
+                  <DeleteQuestionButton
+                    assessmentId={content.id!}
+                    questionId={question.itemId}
+                    num={question.number}
+                  />
+                </div>
+              </>
+            )}
+            {question.type === "CLOZE" && (
+              <>
+                <ClozeQuestionPreview _question={question} />
+                <div className="flex">
+                  <EditClozeQuestionButton
+                    _allRecords={query}
+                    _question={question}
+                    assessmentId={content.id}
+                    courseId={courseId}
+                    item={getItem(question.itemId)}
+                  />
+                  <DeleteQuestionButton
+                    assessmentId={content.id}
+                    questionId={question.itemId}
+                    num={question.number}
+                  />
+                </div>
+              </>
+            )}
+            {question.type === "ASSOCIATION" && (
+              <>
+                <AssociationQuestionPreview _question={question} />
+                <div className="flex">
+                  <EditAssociationQuestionButton
+                    _allRecords={query}
+                    _question={question}
+                    assessmentId={content.id}
+                    courseId={courseId}
+                    item={getItem(question.itemId)}
+                  />
+                  <DeleteQuestionButton
+                    assessmentId={content.id}
+                    questionId={question.itemId}
+                    num={question.number}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+        ))}
+
+        <div className="mt-8 flex flex-col items-start">
+          <AddQuestionButton
+            _allRecords={query}
+            assessmentId={content.id}
+            courseId={courseId}
+          />
+        </div>
+
+        <QuizModal
+          onClose={() => setEditSetOpen(false)}
+          isOpen={isEditSetOpen}
+          _existingQuiz={quiz}
+          chapterId={content.metadata.chapterId}
+        />
+      </ErrorContext.Provider>
     </main>
   );
 }
---
+++ components/quiz/LecturerQuizHeader.tsx
@@ -0,0 +1,104 @@
+import { useError } from "@/app/courses/[courseId]/flashcards/[flashcardSetId]/lecturer";
+import { Delete, Edit } from "@mui/icons-material";
+import { Button, CircularProgress } from "@mui/material";
+import { useParams } from "next/navigation";
+import router from "next/router";
+import { useFragment, useMutation } from "react-relay";
+import { graphql } from "relay-runtime";
+import { ContentTags } from "../ContentTags";
+import { Heading } from "../Heading";
+import { LecturerQuizHeaderDeleteQuizMutation } from "@/__generated__/LecturerQuizHeaderDeleteQuizMutation.graphql";
+import { LecturerQuizHeaderFragment$key } from "@/__generated__/LecturerQuizHeaderFragment.graphql";
+import { FormErrors } from "../FormErrors";
+
+const deleteQuizMutation = graphql`
+  mutation LecturerQuizHeaderDeleteQuizMutation($id: UUID!) {
+    mutateContent(contentId: $id) {
+      deleteContent
+    }
+  }
+`;
+
+const metadataFragment = graphql`
+  fragment LecturerQuizHeaderFragment on Content {
+    id
+    metadata {
+      name
+      ...ContentTags
+    }
+  }
+`;
+
+interface Props {
+  content: LecturerQuizHeaderFragment$key;
+  openEditQuizModal: () => void;
+}
+
+const LecturerQuizHeader = ({ content, openEditQuizModal }: Props) => {
+  const { courseId } = useParams();
+
+  const [commitDeleteQuiz, isDeleteCommitInFlight] =
+    useMutation<LecturerQuizHeaderDeleteQuizMutation>(deleteQuizMutation);
+
+  const { id, metadata } = useFragment(metadataFragment, content);
+
+  const { error, setError } = useError();
+
+  return (
+    <>
+      <Heading
+        title={metadata.name}
+        action={
+          <div className="flex gap-2">
+            <Button
+              sx={{ color: "text.secondary" }}
+              startIcon={
+                isDeleteCommitInFlight ? (
+                  <CircularProgress size={16} />
+                ) : (
+                  <Delete />
+                )
+              }
+              onClick={() => {
+                if (
+                  confirm(
+                    "Do you really want to delete this quiz? This can't be undone."
+                  )
+                ) {
+                  commitDeleteQuiz({
+                    variables: { id: id },
+                    onCompleted: () => {
+                      router.push(`/courses/${courseId}`);
+                    },
+                    onError: (error) => setError(error),
+                    updater: (store) => {
+                      // store.get(id)?.invalidateRecord();
+                      store.delete(id);
+                    },
+                  });
+                }
+              }}
+            >
+              Delete Quiz
+            </Button>
+
+            <Button
+              sx={{ color: "text.secondary" }}
+              startIcon={<Edit />}
+              onClick={openEditQuizModal}
+            >
+              Edit
+            </Button>
+          </div>
+        }
+        backButton
+      />
+
+      <ContentTags metadata={metadata} />
+
+      <FormErrors error={error} onClose={() => setError(null)} />
+    </>
+  );
+};
+
+export default LecturerQuizHeader;
