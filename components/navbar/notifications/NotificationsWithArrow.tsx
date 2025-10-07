a1525886af3619a9d70107461e54406361905c65
rost <cornelius.rost@web.de>
2025-10-07 19:22:02 +0200

    Add these files

---
+++ components/navbar/notifications/NotificationsWithArrow.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import NotificationPopOver from "./NotificationPopOver";
+
+type Props = {
+  anchorEl: HTMLElement | null;
+  setAnchorEl: (el: HTMLElement | null) => void;
+  notifications: any[] | ReadonlyArray<any> | undefined | null;
+  setNotifications: React.Dispatch<React.SetStateAction<any[]>>;
+};
+
+export default function NotificationsWithArrow({
+  anchorEl,
+  setAnchorEl,
+  notifications,
+  setNotifications,
+}: Props) {
+  const list = Array.isArray(notifications) ? [...notifications] : [];
+  return (
+    <NotificationPopOver
+      anchorEl={anchorEl}
+      setAnchorEl={setAnchorEl}
+      notifications={list}
+      setNotifications={setNotifications}
+    />
+  );
+}
