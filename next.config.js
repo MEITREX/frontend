8eb134e295eaaecc3de52b28a0176374f524c8b8
c-neli-r <cornelius.rost@web.de>
2025-12-07 17:23:16 +0100

    Add packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+ transpilePackages: [
+  // 1. Hylimo Pakete
+  "@hylimo/diagram-common",
+  "@hylimo/diagram-protocol",
+  "@hylimo/diagram-ui",
+  "@hylimo/core",
+  "@hylimo/diagram-render-pdf",
+  "@hylimo/diagram-render-svg",
+  "@hylimo/language-server",
+
+  // 2. Monaco & VSCode Language Client
+  "monaco-languageclient",
+  "vscode-languageclient",
+  "vscode-languageserver",
+  "vscode-languageserver-protocol",
+  "vscode-languageserver-types",
+  "vscode-jsonrpc",
+
+],
+
+
+
   output: "standalone",
 
   compiler: {
