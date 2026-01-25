0342a8209837f848c24abdb6cfbc0a9a3198cc34
c-neli-r <cornelius.rost@web.de>
2026-01-25 18:38:43 +0100

    Add api

---
+++ components/hylimo/api/UmlApi.ts
@@ -0,0 +1,29 @@
+﻿import { graphql } from "react-relay";
+
+export const umlApiCreateAssessmentMutation = graphql`
+    mutation UmlApiCreateAssessmentMutation(
+        $assessmentInput: CreateAssessmentInput!,
+        $createUmlExerciseInput: CreateUmlExerciseInput!
+    ) {
+        createUMLAssessment(
+            assessmentInput: $assessmentInput,
+            createUmlExerciseInput: $createUmlExerciseInput
+        ) {
+            id
+        }
+    }
+`;
+
+
+export const umlApiGetUmlExerciseByAssessmentId = graphql`
+  query UmlApiGetUmlExerciseByAssessmentIdQuery($assessmentId: UUID!) {
+      getUmlExerciseByAssessmentId(assessmentId: $assessmentId) {
+        ... on UmlExercise {
+          id
+          description
+          totalPoints
+          tutorSolution
+        }
+    }
+  }
+`;
